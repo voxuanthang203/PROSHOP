f836b46e05438963e1d47f1dd6db118dd3d4322d
Jason Vo <102493073+voxuanthang203@users.noreply.github.com>
2023-09-16 23:32:14 -0400

    Implementing React Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,31 @@
 import { Container } from "react-bootstrap";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Use Routes instead of Route
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Header />
-      <main class="py-3">
+      <main className="py-3">
         <Container>
-          <HomeScreen />
+          <Routes>
+            {" "}
+            {/* Use Routes to wrap your routes */}
+            <Route path="/" element={<HomeScreen />} exact />{" "}
+            {/* Use element prop instead of component */}
+            <Route path="/home" element={<HomeScreen />} exact />{" "}
+            {/* Use element prop instead of component */}
+            <Route path="/product/:id" element={<ProductScreen />} />{" "}
+            {/* Use element prop instead of component */}
+          </Routes>
         </Container>
       </main>
       <Footer />
-    </div>
+    </Router>
   );
 }
 
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,23 +1,28 @@
 import React from "react";
 import { Container, Nav, Navbar, Row } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
 function Header() {
   return (
     <header>
-      <Navbar
-        expand="lg"
-        bg="dark"
-        variant="dark"
-        collapseOnSelect
-      >
+      <Navbar expand="lg" bg="dark" variant="dark" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="#home">PROSHOP</Navbar.Brand>
+          <LinkContainer to="/">
+            <Navbar.Brand>PROSHOP</Navbar.Brand>
+          </LinkContainer>
+
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto">
-              <Nav.Link href="#cart"><i className="fas fa-shopping-cart"></i>Cart</Nav.Link>
-              <Nav.Link href="#login"><i className="fas fa-user"></i>Login</Nav.Link>
-            </Nav>
+            <LinkContainer to="/cart">
+              <Nav.Link>
+                <i className="fas fa-shopping-cart"></i>Cart
+              </Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/login">
+              <Nav.Link>
+                <i className="fas fa-user"></i>Login
+              </Nav.Link>
+            </LinkContainer>
           </Navbar.Collapse>
         </Container>
       </Navbar>
